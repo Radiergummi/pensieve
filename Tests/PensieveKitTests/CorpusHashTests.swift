07e4e2edd2bd5d647b5f42aed7650229f1aea6d2
Moritz Mazetti <moritz@matchory.com>
2026-07-11 13:47:31 +0200

    test(eval): pin CorpusHash boundary-sensitivity

--- Tests/PensieveKitTests/CorpusHashTests.swift
+++ Tests/PensieveKitTests/CorpusHashTests.swift
@@ -8,4 +8,6 @@ import Foundation
   #expect(CorpusHash.hash([a, b]) == CorpusHash.hash([a, b]))
   #expect(CorpusHash.hash([a, b]) != CorpusHash.hash([b, a]))
   #expect(CorpusHash.hash([a, b]).count == 64) // hex sha256
+  // boundary-sensitive: length-prefix must separate parts, so re-splitting the same bytes differs
+  #expect(CorpusHash.hash([Data("ab".utf8), Data("c".utf8)]) != CorpusHash.hash([Data("a".utf8), Data("bc".utf8)]))
 }
